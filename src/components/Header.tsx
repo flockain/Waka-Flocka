9a8f0343ba212671a3df41cc423c7f44109b707e
flockain <simp2earn@gmail.com>
2025-02-26 19:13:27 -0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -70,7 +70,7 @@ const Header = (): JSX.Element => {
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
-              xmlns="[http://www.w3.org/2000/svg"](http://www.w3.org/2000/svg")
+              xmlns="http://www.w3.org/2000/svg"
             >
               {mobileMenuOpen ? (
                 <path
